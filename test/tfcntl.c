6217d2963abec33321d6d08918811cbdc9ed576c
Jim Garlick <garlick.jim@gmail.com>
2010-02-20 00:44:42 +0000

    checkpoint test suite work

---
+++ test/tfcntl.c
@@ -0,0 +1,48 @@
+/* tfcntl.c - verify that fcntl locking works per-pthread like per-process */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+#include "diod_log.h"
+
+#include "test.h"
+
+#define TEST_UID 100
+#define TEST_GID 100
+
+typedef enum { S0, S1, S2, S3, S4, S5 } state_t;
+
+static state_t         state = S0;
+static pthread_mutex_t state_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t  state_cond = PTHREAD_COND_INITIALIZER;
+
+static void
+change_state (state_t s)
+{
+    _lock (&state_lock);
+    state = s;
+    _condsig (&state_cond);
+    _unlock (&state_lock);
+}
+
+static void
+wait_state (state_t s)
+{
+    _lock (&state_lock);
+    while ((state != s))
+        _condwait (&state_cond, &state_lock);
+    _unlock (&state_lock);
+}
+
+
+int
+main (int arg, char *argv[])
+{
+}
