02eb4de8563414c486fa9214696d448401806805
Jim Garlick <garlick.jim@gmail.com>
2010-06-02 22:18:42 +0000

    fix spontaneous test failure in test #9

--- test/tstatfs.c
+++ test/tstatfs.c
@@ -5,16 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+static char *field = NULL;
+
+int
+tfield (char *s)
+{
+    return (!field || !strcmp (field, s));
+}
+
 int
 main (int argc, char *argv[])
 {
     struct statfs s1, s2;
     int diff = 0;
 
-    if (argc != 3) {
-        fprintf (stderr, "Usage: tstatfs dir1 dir2\n");
+    if (argc != 3 && argc != 4) {
+        fprintf (stderr, "Usage: tstatfs dir1 dir2 [field]\n");
         exit (1);
     }
+    if (argc == 4)
+        field = argv[3];
 
     if (statfs (argv[1], &s1) < 0) {
         perror (argv[1]);
@@ -25,39 +35,39 @@ main (int argc, char *argv[])
         exit (1);
     }
 
-    if (s1.f_type != s2.f_type) {
+    if (tfield ("type") && s1.f_type != s2.f_type) {
         fprintf (stderr, "f_type differs\n");
         diff++;
     }
-    if (s1.f_bsize != s2.f_bsize ) {
+    if (tfield ("bsize") && s1.f_bsize != s2.f_bsize ) {
         fprintf (stderr, "f_bsize differs\n");
         diff++;
     }
-    if (s1.f_blocks != s2.f_blocks) {
+    if (tfield ("blocks") && s1.f_blocks != s2.f_blocks) {
         fprintf (stderr, "f_blocks differs\n");
         diff++;
     }
-    if (s1.f_bfree != s2.f_bfree) {
+    if (tfield ("bfree") && s1.f_bfree != s2.f_bfree) {
         fprintf (stderr, "f_bfree differs\n");
         diff++;
     }
-    if (s1.f_bavail != s2.f_bavail) {
+    if (tfield ("bavail") && s1.f_bavail != s2.f_bavail) {
         fprintf (stderr, "f_bavail differs\n");
         diff++;
     }
-    if (s1.f_files != s2.f_files) {
+    if (tfield ("files") && s1.f_files != s2.f_files) {
         fprintf (stderr, "f_files differs\n");
         diff++;
     }
-    if (s1.f_ffree != s2.f_ffree) {
+    if (tfield ("ffree") && s1.f_ffree != s2.f_ffree) {
         fprintf (stderr, "f_ffree differs\n");
         diff++;
     }
-    if (memcmp (&s1.f_fsid, &s2.f_fsid, sizeof (s1.f_fsid)) != 0) {
+    if (tfield ("fsid") && memcmp (&s1.f_fsid, &s2.f_fsid, sizeof (s1.f_fsid)) != 0) {
         fprintf (stderr, "f_fsid differs\n");
         diff++;
     }
-    if (s1.f_namelen != s2.f_namelen) {
+    if (tfield ("namelen") && s1.f_namelen != s2.f_namelen) {
         fprintf (stderr, "f_namelen differs\n");
         diff++;
     }
