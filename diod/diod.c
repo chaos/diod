6606f2bb01b4b0e880988d9e7865e07df0d559c8
Jim Garlick <garlick.jim@gmail.com>
2012-02-14 15:35:41 -0800

    Turn off flush debugging

--- diod/diod.c
+++ diod/diod.c
@@ -585,7 +585,6 @@ _service_run (srvmode_t mode, int rfdno, int wfdno)
     umask (0);
 
     flags |= SRV_FLAGS_DEBUG_FIDPOOL;   /* XXX temporarily on */
-    flags |= SRV_FLAGS_DEBUG_FLUSH;     /* XXX temporarily on */
     flags |= SRV_FLAGS_LOOSEFID;        /* work around buggy clients */
     
     flags |= SRV_FLAGS_AUTHCONN;
