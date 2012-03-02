dfad45dd625991ad8d02e22eea2cb14a6cdd9209
Jim Garlick <garlick.jim@gmail.com>
2012-03-02 11:43:19 -0800

    Add misc/t13 server valgrind test
    Adds coverage to include diod-specific portions of the code
    compared to misc/t11.

---
+++ tests/misc/tnpsrv2.c
@@ -0,0 +1,213 @@
+/* tnpsrv2.c - test client/server with diod ops (valgrind me) */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <assert.h>
+
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+
+#include "9p.h"
+#include "npfs.h"
+#include "npclient.h"
+
+#include "list.h"
+#include "diod_log.h"
+#include "diod_conf.h"
+#include "diod_sock.h"
+
+#include "ops.h"
+
+#define TEST_MSIZE 8192
+
+#define TEST_ITER 64
+
+int
+main (int argc, char *argv[])
+{
+    Npsrv *srv;
+    int s[2];
+    int flags = 0;
+    Npcfsys *fs;
+    Npcfid *root, *f[TEST_ITER];
+    char tmpdir[] = "/tmp/tnpsrv2.XXXXXX";
+    int i;
+    char tmpstr[PATH_MAX + 1];
+    int n, len = 4096*100;
+    char *buf = malloc (len);
+    char *buf2 = malloc (len);
+
+
+    diod_log_init (argv[0]);
+    diod_conf_init ();
+    diod_conf_set_auth_required (0);
+
+    /* create export */
+    if (!mkdtemp (tmpdir))
+        err_exit ("mkdtemp");
+    diod_conf_add_exports (tmpdir);
+    diod_conf_set_exportopts ("sharefd");
+
+    if (socketpair (AF_LOCAL, SOCK_STREAM, 0, s) < 0)
+        err_exit ("socketpair");
+
+    if (!(srv = np_srv_create (16, flags)))
+        errn_exit (np_rerror (), "np_srv_create");
+    if (diod_init (srv) < 0)
+        errn_exit (np_rerror (), "diod_init");
+    diod_sock_startfd (srv, s[1], s[1], "loopback");
+
+    if (!(fs = npc_start (s[0], s[0], TEST_MSIZE, 0)))
+        errn_exit (np_rerror (), "npc_start");
+
+    if (!(root = npc_attach (fs, NULL, tmpdir, 0)))
+        errn_exit (np_rerror (), "npc_attach");
+    msg ("attached");
+
+    /* create a file */
+    if (!(f[0] = npc_create_bypath (root, "/foo", 0, 0644, getgid())))
+        errn (np_rerror (), "npc_create_bypath /foo");
+    else {
+        if (!buf || !buf2)
+            errn_exit (ENOMEM, "malloc");
+        msg ("created file");
+        if (npc_clunk (f[0]) < 0)
+            errn (np_rerror (), "npc_clunk");
+
+        /* fill it with some data */
+        memset (buf, 9, len);
+        n = npc_put (root, "/foo", buf, len);
+        if (n < 0)
+            errn (np_rerror (), "npc_put");
+        else
+            msg ("write %d bytes", n);
+
+        /*  walk */
+        for (i = 0; i < TEST_ITER; i++) {
+            f[i] = npc_walk (root, "foo");
+            if (!f[i])
+                errn (np_rerror (), "npc_walk");
+            else
+                msg ("walk");
+        }
+        /* open */
+        for (i = 0; i < TEST_ITER; i++) {
+            if (f[i]) {
+                if (npc_open (f[i], O_RDONLY) < 0)
+                    errn (np_rerror (), "npc_open");
+                else
+                    msg ("open");
+            }
+        }
+        /* read (using new fids) */
+        for (i = 0; i < TEST_ITER; i++) {
+            if (f[i]) {
+                n = npc_get (root, "/foo", buf2, len);
+                if (n < 0)
+                    errn (np_rerror (), "npc_get");
+                else
+                    msg ("read %d bytes", n);
+                if (memcmp (buf, buf2, len) != 0)
+                    msg ("memcmp failure");
+            }
+        }
+        /* clunk */
+        for (i = 0; i < TEST_ITER; i++) {
+            if (f[i]) {
+                if (npc_clunk (f[i]) < 0)
+                    errn (np_rerror (), "npc_clunk");
+                else
+                    msg ("clunk");
+            }
+        }
+
+        /* remove the file
+         * N.B. npc_remove not implemented yet so we have this reach around
+         */
+        snprintf (tmpstr, sizeof(tmpstr), "%s/foo", tmpdir);
+        if (unlink (tmpstr) < 0)
+            errn (errno, "unlink");
+        else
+            msg ("unlink");
+
+        free (buf);
+        free (buf2);
+    }
+
+    /* create a directory */
+    if (npc_mkdir_bypath (root, "foo", 0755) < 0)
+        errn (errno, "npc_mkdir_bypath");
+    else {
+        msg ("mkdir");
+
+        /* create files */
+        for (i = 0; i < TEST_ITER; i++) {
+            snprintf (tmpstr, sizeof (tmpstr), "/foo/%i", i);
+            if (!(f[i] = npc_create_bypath (root, tmpstr, 0, 0644, getgid())))
+                errn (np_rerror (), "npc_create_bypath %s", tmpstr);
+            else
+                msg ("created %s", tmpstr);
+        }
+        /* clunk */
+        for (i = 0; i < TEST_ITER; i++) {
+            if (f[i]) {
+                if (npc_clunk (f[i]) < 0)
+                    errn (np_rerror (), "npc_clunk");
+                else
+                    msg ("clunk");
+            }
+        }
+        /* unlink files */
+        for (i = 0; i < TEST_ITER; i++) {
+            snprintf (tmpstr, sizeof (tmpstr), "%s/foo/%i", tmpdir, i);
+            if (unlink (tmpstr) < 0)
+                errn (errno, "unlink");
+            else
+                msg ("unlink");
+        }
+
+        /* remove directory */
+        snprintf (tmpstr, sizeof(tmpstr), "%s/foo", tmpdir);
+        if (rmdir (tmpstr) < 0)
+            errn (errno, "rmdir");
+        else
+            msg ("rmdir");
+    }
+
+    npc_clunk (root);
+
+    npc_finish (fs);
+
+    np_srv_wait_conncount (srv, 1);
+
+    /* N.B. The conn reader thread runs detached and signals us as it is
+     * about to exit.  If we manage to wake up and exit first, valgrind
+     * reports the reader's tls as leaked.  Add the sleep to work around
+     * this race for now.
+     */
+    sleep (1);
+
+    diod_fini (srv);
+    np_srv_destroy (srv);
+
+    rmdir (tmpdir);
+
+    diod_conf_fini ();
+    diod_log_fini ();
+    exit (0);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
