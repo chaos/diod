4fc69f38becda4b2049d6453686e8dad84bc2666
Jim Garlick <garlick.jim@gmail.com>
2013-05-02 08:53:51 -0700

    xattr/acl tests depend on Kernel/backing fs support

---
+++ tests/user/txattr.c
@@ -0,0 +1,49 @@
+/* txattr.c - test if a file system supports extended attributes */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <attr/xattr.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <libgen.h>
+
+#include "diod_log.h"
+
+
+static void
+usage (void)
+{
+    fprintf (stderr, "Usage: txattr path\n");
+    exit (1);
+}
+
+int
+main (int argc, char *argv[])
+{
+    char *path;
+
+    diod_log_init (argv[0]);
+
+    if (argc != 2)
+        usage ();
+    path = argv[1];
+
+    if (getxattr (path, "user.foo", NULL, 0) && errno == ENOTSUP)
+        exit (1);
+    exit (0);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
