6b398b33bee1d795f18c29c76c795b9b244f952d
Jim Garlick <garlick.jim@gmail.com>
2013-05-07 19:17:23 -0700

    add atomic create test

---
+++ tests/kern/tatomic.c
@@ -0,0 +1,30 @@
+/* tatomic.c - test O_CREAT|O_EXCL atomic create */
+
+/* posted to v9fs-developer by M. Mohan Kumar */
+
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+        int fd;
+
+        /* first create the file */
+        fd = open(argv[1], O_CREAT|O_WRONLY);
+        if (fd < 0) {
+                perror("open");
+                return -1;
+       	}
+	close(fd);
+
+        /* Now opening same file with O_CREAT|O_EXCL should fail */
+        fd = open(argv[1], O_CREAT|O_EXCL);
+        if (fd < 0 && errno == EEXIST)
+                printf("test case pass\n");
+        else
+            	printf("test case failed\n");
+        close(fd);
+        return 0;
+}
+
