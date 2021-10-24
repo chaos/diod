d5444ce82dcb6d3ab512dd3345cf9cc88fc5d229
Jim Garlick <garlick.jim@gmail.com>
2021-10-24 15:47:44 -0700

    license: update 9p.h kernel header
    Problem: 9p.h came from an earlier linux kernel,
    before they converted to SPDX identifiers.
    
    Make the header look like the one upstream at
    https://github.com/torvalds/linux/blob/master/include/net/9p/9p.h

--- libnpfs/9p.h
+++ libnpfs/9p.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * include/net/9p/9p.h
  *
@@ -6,23 +7,7 @@
  *  Copyright (C) 2005 by Latchesar Ionkov <lucho@ionkov.net>
  *  Copyright (C) 2004 by Eric Van Hensbergen <ericvh@gmail.com>
  *  Copyright (C) 2002 by Ron Minnich <rminnich@lanl.gov>
- *  Copyright (C) 2010-14 Lawrence Livermore National Security, LLC
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2
- *  as published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to:
- *  Free Software Foundation
- *  51 Franklin Street, Fifth Floor
- *  Boston, MA  02111-1301  USA
- *
+ *  Copyright (C) 2010 Lawrence Livermore National Security, LLC
  */
 
 /* Copied from 2.6.38-rc2 kernel, then modified for use with diod.
