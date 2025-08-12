6150ba0030f481eca9b3f4d936ae3b1c06106499
Jim Garlick <garlick.jim@gmail.com>
2025-08-12 07:02:13 -0700

    testsuite: make tattach look like other test progs
    Problem: tattach expects to be run with a server on stdin,
    but that doesn't work with test_under_diod().
    
    Add test program boiler plate which should let it run
    in test_under_diod() and other circumstances.

--- src/cmd/test/tattach.c
+++ src/cmd/test/tattach.c
@@ -8,7 +8,14 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
 \************************************************************/
 
-/* tattachmt.c - simulate multiple simultaneous mout requests */
+/* tattachmt.c - simulate multiple simultaneous mount requests
+ *
+ * When run with numthreads > 1 this selects multithreaded mtfsys
+ * in libnpclient, which prevents multiple threads from accessing the
+ * server fd simultaneously.  Unfortunately it has a bug that causes
+ * the next thing run in test_under_diod() mode to fail with a
+ * protocol error.  That needs to be run down.
+ */
 
 #if HAVE_CONFIG_H
 #include "config.h"
@@ -16,9 +23,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdarg.h>
+#include <getopt.h>
 #include <errno.h>
 #include <stdint.h>
 #include <inttypes.h>
@@ -30,8 +39,9 @@
 
 #include "src/libdiod/diod_log.h"
 #include "src/libdiod/diod_auth.h"
+#include "src/libdiod/diod_sock.h"
+#include "src/libdiod/diod_auth.h"
 
-const int fd = 0; /* stdin */
 
 typedef struct {
     Npcfsys *fs;
@@ -42,12 +52,27 @@ typedef struct {
     pthread_t t;
 } thd_t;
 
-static void
-usage (void)
+void usage (void)
 {
-    fprintf (stderr, "Usage: tattachmt numusers numthreads numgetattrs iterations aname\n");
+    fprintf (stderr, "Usage: tattach [OPTIONS] numusers numthreads numgetattrs iterations\n"
+"   -a,--aname NAME       file system (default ctl)\n"
+"   -s,--server HOST:PORT server (default localhost:564)\n"
+"   -m,--msize            msize (default 65536)\n"
+"   -t,--timeout SECS     give up after specified seconds\n"
+"   -p,--privport         connect from a privileged port (root user only)\n"
+);
     exit (1);
 }
+
+static const char *options = "a:s:m:p";
+
+static const struct option longopts[] = {
+    {"aname",   required_argument,      0, 'a'},
+    {"server",  required_argument,      0, 's'},
+    {"msize",   required_argument,      0, 'm'},
+    {"privport",no_argument,            0, 'p'},
+    {0, 0, 0, 0},
+};
 
 /* attach to aname and issue a getattr,
  * similar to what a v9fs mount looks like to the server.
@@ -87,26 +112,65 @@ done:
 int
 main (int argc, char *argv[])
 {
+    char *aname = NULL;
+    char *server = NULL;
+    int msize = 65536;
+    int flags = 0;
+    int server_fd;
+    bool not_my_serverfd = false;
     Npcfsys *fs;
-    char *aname;
+    int npcflags = 0;
     thd_t *t;
     int i, err, numthreads, numusers, numgetattrs, iterations;
     uid_t *uids;
     struct passwd *pw;
+    int c;
 
     diod_log_init (argv[0]);
 
-    if (argc != 6)
+    opterr = 0;
+    while ((c = getopt_long (argc, argv, options, longopts, NULL)) != -1) {
+        switch (c) {
+            case 'a':   /* --aname NAME */
+                aname = optarg;
+                break;
+            case 's':   /* --server HOST[:PORT] or /path/to/socket */
+                server = optarg;
+                break;
+            case 'm':   /* --msize SIZE */
+                msize = strtoul (optarg, NULL, 10);
+                break;
+            case 'p':   /* --privport */
+                flags |= DIOD_SOCK_PRIVPORT;
+                break;
+            default:
+                usage ();
+        }
+    }
+    if (argc - optind != 4)
         usage ();
-    numusers = strtoul (argv[1], NULL, 10);
-    numthreads = strtoul (argv[2], NULL, 10);
-    numgetattrs = strtoul (argv[3], NULL, 10);
-    iterations = strtoul (argv[4], NULL, 10);
-    aname = argv[5];
 
-    if (!(t = malloc (sizeof(*t) * numthreads)))
-        msg_exit ("out of memory");
-    if (!(uids = malloc (sizeof(*uids) * numusers)))
+    numusers = strtoul (argv[optind++], NULL, 10);
+    numthreads = strtoul (argv[optind++], NULL, 10);
+    numgetattrs = strtoul (argv[optind++], NULL, 10);
+    iterations = strtoul (argv[optind++], NULL, 10);
+
+    const char *s = getenv ("DIOD_SERVER_FD");
+    if (server || !s) {
+        server_fd = diod_sock_connect (server, flags);
+        if (server_fd < 0)
+            exit (1);
+    }
+    else {
+        server_fd = strtoul (s, NULL, 10);
+        not_my_serverfd = true;
+    }
+    if (!aname)
+        aname = getenv ("DIOD_SERVER_ANAME");
+
+    t = calloc (numthreads, sizeof (t[0]));
+    uids = calloc (numusers, sizeof (uids[0]));
+    if (!t || !uids)
         msg_exit ("out of memory");
 
     if (numusers > 1) {
@@ -123,7 +187,9 @@ main (int argc, char *argv[])
         msg_exit ("numusers must be >= 1");
     }
 
-    if (!(fs = npc_start (fd, fd, 8192+24, NPC_MULTI_RPC)))
+    if (numthreads > 1)
+        npcflags |= NPC_MULTI_RPC;
+    if (!(fs = npc_start (server_fd, server_fd, msize, npcflags)))
         errn_exit (np_rerror (), "npc_start");
 
     for (i = 0; i < numthreads; i++) {
@@ -142,6 +208,12 @@ main (int argc, char *argv[])
     }
 
     npc_finish (fs);
+
+    if (!not_my_serverfd)
+        close (server_fd);
+
+    free (t);
+    free (uids);
 
     diod_log_fini ();
 
