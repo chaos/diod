6e12930778d152f4a00402d05310ab7086ac81b2
Jim Garlick <garlick.jim@gmail.com>
2025-10-29 12:35:41 -0700

    scripts: update install-deps-deb.sh
    Problem: install-deps-deb.sh doesn't install all the test dependencies.
    
    Add optional package set arguments of "test" and "deb" to get
    the packages needed for testing and test deb building, respectively.
    
    Now you can run "sudo ./install-deps-deb.sh test deb"

--- scripts/install-deps-deb.sh
+++ scripts/install-deps-deb.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# Note:
+# stat(1) is in coreutils
+# flock(1) is in util-linux
+# These are probably already installed
+
+EXTRA_PKGS=""
+for pkgset in $*; do
+	case $pkgset in
+	test)
+	    EXTRA_PKGS="$EXTRA_PKGS valgrind acl attr scrub rsync postmark dbench"
+	    ;;
+	deb)
+	    EXTRA_PKGS="$EXTRA_PKGS devscripts debhelper"
+	    ;;
+	esac
+done
+
 apt install \
   autoconf \
   automake \
@@ -10,6 +27,4 @@ apt install \
   libcap2-dev \
   lua5.1 \
   liblua5.1-dev \
-  valgrind \
-
-# to build test DEBs you need devscripts and debhelper too
+  $EXTRA_PKGS
