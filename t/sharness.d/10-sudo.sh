221e0ec4fed7232cf824919e78f53d98145634f0
Jim Garlick <garlick.jim@gmail.com>
2025-11-07 08:59:01 -0800

    sharness: use doas(8) if sudo(8) is unavailable
    Problem: on some systems like alpine linux and the BSDs,
    doas(8) is preferred over sudo(8).
    
    As long as doas(8) is configured for "keepenv" and "nopass",
    allow it to be used.

--- t/sharness.d/10-sudo.sh
+++ t/sharness.d/10-sudo.sh
@@ -4,11 +4,14 @@
 if sudo --non-interactive true >/dev/null 2>&1; then
     test_set_prereq SUDO
     SUDO="sudo -E"
+elif _probeenv=xyz doas -n printenv _probeenv >/dev/null 2>&1; then
+    test_set_prereq SUDO
+    SUDO="doas"
 fi
 ##
 # Fixup sudo commandline if sanitizers are enabled.
 # LSan doesn't work under setuid or program run under sudo
 ##
 if test_have_prereq ASAN; then
-    SUDO="sudo -E ASAN_OPTIONS=$ASAN_OPTIONS:detect_leaks=0"
+    SUDO="ASAN_OPTIONS=$ASAN_OPTIONS:detect_leaks=0 $SUDO"
 fi
